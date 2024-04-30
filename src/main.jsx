42de2dc80c03f769043d027e86bfaf156aedeeb9
Enrick Santos de Souza <enrick.santos@makevendas.com.br>
2024-04-29 21:05:16 -0300

    criado pagina de checkout

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home from './Home/Home'
-import Login from './pages/login/Login'
-import { AuthProvider } from './contexts/auth'
-import SignUp from './pages/signUp/SignUp'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './Home/Home';
+import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/auth';
+import SignUp from './pages/signUp/SignUp';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Checkout from './pages/checkout/Checkout';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/cadastrar',
-        element: <SignUp />
-    },
-])
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/cadastrar',
+    element: <SignUp />,
+  },
+  {
+    path: '/checkout',
+    element: <Checkout />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <AuthProvider>
-        <React.StrictMode>
-            <RouterProvider router={router} />
-        </React.StrictMode>
-    </AuthProvider>
-)
+  <AuthProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </AuthProvider>,
+);

dccfbc2467a5e2bb8c0b1b25336c8f42f1fda840
enrick-dev <enricksantos03@gmail.com>
2024-06-30 21:15:48 -0300

    remoção de texto na home

--- src/pages/Home/components/Body/Body.jsx
+++ src/pages/Home/components/Body/Body.jsx
@@ -4,37 +4,19 @@ import CardProduto from './CardProduto';
 const Body = () => {
   const { data: orders } = useOrdersBussines();
   return (
-    <div className="py-10 px-5 flex flex-col gap-6">
-      <h2 className="text-2xl text-center">
-        <div className="text-center">
-          <span className="block text-4xl font-extrabold uppercase text-primary-700 leading-none">
-            Faça sua escolha
-          </span>
-          <span className="block text-xl font-light lowercase text-primary-400 leading-none">
-            e
-          </span>
-          <span className="block text-2xl font-medium uppercase text-primary-500 leading-none">
-            adicione no seu carrinho
-          </span>
-          <span className="block text-3xl font-bold uppercase text-primary-600 leading-none">
-            decompras
-          </span>
-        </div>
-      </h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        {orders &&
-          !orders.error &&
-          orders.map(({ id, name, description, price, photo_thumb }, i) => (
-            <CardProduto
-              key={i}
-              id={id}
-              name={name}
-              description={description}
-              price={price}
-              photo_thumb={photo_thumb}
-            />
-          ))}
-      </div>
+    <div className="py-10 px-5 flex flex-wrap justify-center gap-4">
+      {orders &&
+        !orders.error &&
+        orders.map(({ id, name, description, price, photo_thumb }, i) => (
+          <CardProduto
+            key={i}
+            id={id}
+            name={name}
+            description={description}
+            price={price}
+            photo_thumb={photo_thumb}
+          />
+        ))}
     </div>
   );
 };
